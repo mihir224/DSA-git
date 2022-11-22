410a74f2fb60a3ae45b709da072d541376fe3e72
mihir224 <mihirsaini25@gmail.com>
2022-11-22 13:37:15 +0530

    added Kruskal

--- Graphs/src/Algorithms.java
+++ Graphs/src/Algorithms.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class Algorithms {
@@ -43,5 +45,47 @@ public class Algorithms {
         return sum;
     }
 
+    //Kruskal
+    class Edge implements Comparable<Edge> { //comparable class is implemented to use the compareTo function to sort
+        // the elements of the edge according to weight
+        int wt;
+        int src;
+        int dest;
+        public Edge(int wt, int src, int dest) {
+            this.wt = wt;
+            this.src = src;
+            this.dest = dest;
+        }
+        public int compareTo(Edge compareEdge) { //sort function of collections makes use of this method
+            return this.wt-compareEdge.wt;
+        }
+    }
+    int spanningTree1(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj)//For a weighted graph, every node in the
+    // adjacency list is in the form of a pair in which first element is the node and second is the weight
+    {
+        List<Edge> edges=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            for(int j=0;j<adj.get(i).size();j++){
+                int adjNode=adj.get(i).get(j).get(0);
+                int weight=adj.get(i).get(j).get(1);
+                int node=i;
+                Edge edge=new Edge(weight,node,adjNode); //creating edges for each node
+                edges.add(edge);
+            }
+        }
+        DisjointSet ds=new DisjointSet(V);
+        Collections.sort(edges); //sorting the list of edges based on their weight
+        int mstWeight=0;
+        for(int i=0;i<edges.size();i++){
+            int weight=edges.get(i).wt;
+            int node=edges.get(i).src;
+            int adjNode=edges.get(i).dest;
+            if(ds.findUltimateParent(node)!=ds.findUltimateParent(adjNode)){ //not in same component
+                ds.unionBySize(node,adjNode);
+                mstWeight+=weight;
+            }
+        }
+        return mstWeight;
+    }
 
 }
