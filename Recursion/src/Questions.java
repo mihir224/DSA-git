4228dfc1b2fad51f42473ef6cb2a3dcdea3c7953
mihir224 <mihirsaini25@gmail.com>
2023-01-09 23:50:11 +0530

    new questions

--- Recursion/src/Questions.java
+++ Recursion/src/Questions.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Questions {
     //combination sum
@@ -22,6 +21,36 @@ public class Questions {
             list.remove(list.size()-1);
         }
         helper(i+1,candidates,target,ans,list);
+    }
+
+    //combination sum 2
+    //https://leetcode.com/problems/combination-sum-ii/
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<Integer> list=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(candidates);
+        helper2(0,candidates,target,list,ans);
+        return ans;
+    }
+    public void helper2(int currentIndex, int[] candidates, int target, List<Integer> list, List<List<Integer>> ans){
+        if(target==0){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<candidates.length;i++){
+            //case 1
+            if(i>currentIndex&&candidates[i]==candidates[i-1]){ //check for duplicates
+                continue;
+            }
+            //case 2
+            if(candidates[i]>target){
+                break;
+            }
+            //case 3 (the one that leads us to possible solution)
+            list.add(candidates[i]);
+            helper2(i+1,candidates,target-candidates[i],list,ans);
+            list.remove(list.size()-1);
+        }
     }
 
 }
