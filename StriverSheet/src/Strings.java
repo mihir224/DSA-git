fa1e1d50173e028f89fa6207333f5a58f14ccd73
mihir224 <mihirsaini25@gmail.com>
2023-03-23 23:59:32 +0530

    new questions

--- StriverSheet/src/Strings.java
+++ StriverSheet/src/Strings.java
@@ -397,4 +397,46 @@ public class Strings {
         return map.isEmpty();
     }
 
+    //count and say
+    //https://leetcode.com/problems/count-and-say/
+    public String countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        String s="1";
+        int count=0;
+        for(int i=2;i<=n;i++){
+            String t="";
+            count=1;
+            s=s+"&"; //here '&' acts as a delimiter so that the last element is included when we've completely traversed the string
+            for(int j=1;j<s.length();j++){
+                if(s.charAt(j)!=s.charAt(j-1)){
+                    t=t+Integer.toString(count);
+                    t=t+s.charAt(j-1);
+                    count=1;
+                }
+                else{
+                    count++;
+                }
+            }
+            s=t;
+        }
+        return s;
+    }
+
+    //compare version numbers
+    //https://leetcode.com/problems/compare-version-numbers/
+    public int compareVersion(String version1, String version2) {
+        int n=version1.length();
+        int m=version2.length();
+        int i=0;
+        int j=0;
+        while(i<n||j<m){ //here we use OR to completely traverse one string if it is greater than the other
+            int num1=0;
+            int num2=0;
+            while(i<n&&version1.charAt(i)!='.'){
+                num1=num1*10+version1.charAt()
+            }
+        }
+    }
 }
